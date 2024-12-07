66325493e7fb2aab8e62cb4171531a8b75462e64
dinelka <newlakshan3@gmail.com>
2024-12-07 19:26:01 +0530

    variable rules

---
+++ Day01/main.py
@@ -0,0 +1,41 @@
+print("Hello python")
+
+#python variable
+#python is dymically typed,can assign any type
+varivable = 10;
+print(varivable)
+
+name = "Dinela"
+print(name)
+
+is_student = True
+print(is_student)
+
+pi_value = 3.14
+print(pi_value)
+
+#rules for naming python variable
+#1)start with letter or underscore character
+valid_name = 10
+print(valid_name)
+
+_new_variable =20;
+print(_new_variable)
+
+#2)cannot start number
+#2variable = 20  # Invalid
+
+#3)Variable names can consist of letters (a–z, A–Z), digits (0–9), and underscores (_).
+_variable_new="Dinelka"
+print(_variable_new)
+
+#4)cannot be used keyword and reserved
+#class = "invalid"  # Invalid
+
+#5) fallow snake case
+has_items = False
+print(has_items)
+
+#4)Constants in Uppercase
+MAX_RETRIES = 12
+print(MAX_RETRIES)
