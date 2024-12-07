e9371d2d9d42beef42fb951ead39bbb29a6bd7bf
dinelka <newlakshan3@gmail.com>
2024-12-07 22:40:39 +0530

    String data type

---
+++ Day01/StringDataType.py
@@ -0,0 +1,23 @@
+str1 ="Dinelka"
+print(str1)
+
+str2 = 'lakshan'
+print(str2)
+
+#Acess String index
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[-1]) # end index
+print("str1[3:4] :",str1[3:4])
+print("str1[3:5] :",str1[3:5])
+print("str1[3:6] :",str1[3:6])
+print("str1[:4] :",str1[:4])
+print("str1[4:] :",str1[4:])
+
+#String concatenation
+s1="python"
+s2="programming"
+s3=45
+print(s1 +" " +s2)
+print(s1 +" " +s2 + " " + str(s3))
